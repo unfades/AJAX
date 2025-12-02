af04dc3a4b505f7ced3f653360c98b9194dde8a3
unfades <179614206+unfades@users.noreply.github.com>
2025-12-02 18:52:47 +0530

    finished half of lecture 60

---
+++ AJAXFromMERN/Lecture60/exercises/exercise2.js
@@ -0,0 +1,27 @@
+let resSpan;
+function displayResult(result){
+    if(typeof result === 'number'){
+        resSpan.innerHTML += `It's square is ${result}`;
+    }else{
+        resSpan.innerHTML += `${result}`;
+    }
+}
+
+function calculateSquare(value, callback){
+    setTimeout(()=>{
+    let num = Number(value);
+    if(isNaN(num)){
+        callback("Cannot calc square of nonnumeric data");
+        return;
+    }
+    callback(num*num);
+    },2000);
+}
+
+function doTask(){
+    let inp = document.getElementById("userinp");
+    resSpan = document.getElementById("result");
+    let data = inp.value;
+    resSpan.innerHTML = `Curr Value: ${data}<br>`;
+    calculateSquare(data,displayResult);
+}
