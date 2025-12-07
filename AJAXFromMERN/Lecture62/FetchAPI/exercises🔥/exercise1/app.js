8df1508b0d0346fbab2842f33c9f57f1187d3aba
unfade <179614206+unfades@users.noreply.github.com>
2025-12-07 19:48:10 +0530

    made changes

--- AJAXFromMERN/Lecture62/FetchAPI/exercises🔥/exercise1/app.js
+++ AJAXFromMERN/Lecture62/FetchAPI/exercises🔥/exercise1/app.js
@@ -1,23 +1,5 @@
-//let unpressed = true;
-//my try
-// function viewData(){
-//     let pr = fetch("https://api.github.com/users/unfades");
-//     let resDiv = document.getElementById("result");
-//     pr.then((response)=>{
-//         return response.json();
-//     }).then((data)=>{
-//         if(unpressed){
-//             resDiv.innerHTML += "name: " + data.name + "<br>";
-//             resDiv.innerHTML += "image: <img src = " + data.avatar_url + " width = 200px></img><br>";
-//             if(data.company === null) resDiv.innerHTML += "company: not available<br>";
-//             else resDiv.innerHTML += "company: " + data.company + "<br>";
-//             if(data.blog === "") resDiv.innerHTML += "website: not available<br>"; 
-//             else resDiv.innerHTML += "website: <a href =" + data.blog + ">"+data.blog+"</a><br>";
-//             unpressed = false;
-//         }
-//     });
-// }
-//sirs proper with DOM
+//my try in appmine.js
+//sirs this is proper with DOM
 let unpressed = true;
 function viewData(){
     let resDiv = document.getElementById("result");
